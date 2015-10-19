b554eb9891aa27b64276cc913a7c45f485cccf52
Raim Interiano <Raimmaster@users.noreply.github.com>
2015-10-19 05:17:14 -0600

    Update

--- DiscreteTechniques/Form1.Designer.cs
+++ DiscreteTechniques/Form1.Designer.cs
@@ -30,14 +30,24 @@
         {
             this.menu = new System.Windows.Forms.MenuStrip();
             this.técnicasDeConteoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsPSR = new System.Windows.Forms.ToolStripMenuItem();
+            this.permutaciónConRepeticiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.variaciónSinRepeticiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.variaciónConRepeticiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.combinaciónSinRepeticiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.combinaciónConRepeticiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.másTécnicasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reglaMultiplicativaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reglaAditivaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.permutaciónCircularToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menu.SuspendLayout();
             this.SuspendLayout();
             // 
             // menu
             // 
             this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.técnicasDeConteoToolStripMenuItem});
+            this.técnicasDeConteoToolStripMenuItem,
+            this.másTécnicasToolStripMenuItem});
             this.menu.Location = new System.Drawing.Point(0, 0);
             this.menu.Name = "menu";
             this.menu.Size = new System.Drawing.Size(599, 24);
@@ -47,10 +57,80 @@
             // técnicasDeConteoToolStripMenuItem
             // 
             this.técnicasDeConteoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem1});
+            this.tsPSR,
+            this.permutaciónConRepeticiónToolStripMenuItem,
+            this.variaciónSinRepeticiónToolStripMenuItem,
+            this.variaciónConRepeticiónToolStripMenuItem,
+            this.combinaciónSinRepeticiónToolStripMenuItem,
+            this.combinaciónConRepeticiónToolStripMenuItem,
+            this.permutaciónCircularToolStripMenuItem});
             this.técnicasDeConteoToolStripMenuItem.Name = "técnicasDeConteoToolStripMenuItem";
             this.técnicasDeConteoToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
-            this.técnicasDeConteoToolStripMenuItem.Text = "Técnicas de Conteo";          
+            this.técnicasDeConteoToolStripMenuItem.Text = "Técnicas de Conteo";
+            // 
+            // tsPSR
+            // 
+            this.tsPSR.Enabled = false;
+            this.tsPSR.Name = "tsPSR";
+            this.tsPSR.Size = new System.Drawing.Size(228, 22);
+            this.tsPSR.Text = "Permutación sin Repetición";
+            // 
+            // permutaciónConRepeticiónToolStripMenuItem
+            // 
+            this.permutaciónConRepeticiónToolStripMenuItem.Name = "permutaciónConRepeticiónToolStripMenuItem";
+            this.permutaciónConRepeticiónToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.permutaciónConRepeticiónToolStripMenuItem.Text = "Permutación con Repetición";
+            // 
+            // variaciónSinRepeticiónToolStripMenuItem
+            // 
+            this.variaciónSinRepeticiónToolStripMenuItem.Name = "variaciónSinRepeticiónToolStripMenuItem";
+            this.variaciónSinRepeticiónToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.variaciónSinRepeticiónToolStripMenuItem.Text = "Variación sin Repetición";
+            // 
+            // variaciónConRepeticiónToolStripMenuItem
+            // 
+            this.variaciónConRepeticiónToolStripMenuItem.Name = "variaciónConRepeticiónToolStripMenuItem";
+            this.variaciónConRepeticiónToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.variaciónConRepeticiónToolStripMenuItem.Text = "Variación con Repetición";
+            // 
+            // combinaciónSinRepeticiónToolStripMenuItem
+            // 
+            this.combinaciónSinRepeticiónToolStripMenuItem.Name = "combinaciónSinRepeticiónToolStripMenuItem";
+            this.combinaciónSinRepeticiónToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.combinaciónSinRepeticiónToolStripMenuItem.Text = "Combinación sin Repetición";
+            // 
+            // combinaciónConRepeticiónToolStripMenuItem
+            // 
+            this.combinaciónConRepeticiónToolStripMenuItem.Name = "combinaciónConRepeticiónToolStripMenuItem";
+            this.combinaciónConRepeticiónToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.combinaciónConRepeticiónToolStripMenuItem.Text = "Combinación con Repetición";
+            // 
+            // másTécnicasToolStripMenuItem
+            // 
+            this.másTécnicasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reglaMultiplicativaToolStripMenuItem,
+            this.reglaAditivaToolStripMenuItem});
+            this.másTécnicasToolStripMenuItem.Name = "másTécnicasToolStripMenuItem";
+            this.másTécnicasToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.másTécnicasToolStripMenuItem.Text = "Más Técnicas";
+            // 
+            // reglaMultiplicativaToolStripMenuItem
+            // 
+            this.reglaMultiplicativaToolStripMenuItem.Name = "reglaMultiplicativaToolStripMenuItem";
+            this.reglaMultiplicativaToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.reglaMultiplicativaToolStripMenuItem.Text = "Regla Multiplicativa";
+            // 
+            // reglaAditivaToolStripMenuItem
+            // 
+            this.reglaAditivaToolStripMenuItem.Name = "reglaAditivaToolStripMenuItem";
+            this.reglaAditivaToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.reglaAditivaToolStripMenuItem.Text = "Regla Aditiva";
+            // 
+            // permutaciónCircularToolStripMenuItem
+            // 
+            this.permutaciónCircularToolStripMenuItem.Name = "permutaciónCircularToolStripMenuItem";
+            this.permutaciónCircularToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.permutaciónCircularToolStripMenuItem.Text = "Permutación Circular";
             // 
             // Form1
             // 
@@ -72,7 +152,16 @@
 
         private System.Windows.Forms.MenuStrip menu;
         private System.Windows.Forms.ToolStripMenuItem técnicasDeConteoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem tsPSR;
+        private System.Windows.Forms.ToolStripMenuItem permutaciónConRepeticiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem variaciónSinRepeticiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem variaciónConRepeticiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem combinaciónSinRepeticiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem combinaciónConRepeticiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem permutaciónCircularToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem másTécnicasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reglaMultiplicativaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reglaAditivaToolStripMenuItem;
     }
 }
 
