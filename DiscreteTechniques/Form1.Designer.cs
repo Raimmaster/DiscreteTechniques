a41c3ecf7049f43f6e820d825ee35fa979a0e724
Raim Interiano <Raimmaster@users.noreply.github.com>
2015-10-18 22:06:34 -0600

    Methods 1-7 added; 8 and 9 pending, as they might require recursion

--- DiscreteTechniques/Form1.Designer.cs
+++ DiscreteTechniques/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.técnicasDeConteoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.técnicasDeConteoToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(319, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // técnicasDeConteoToolStripMenuItem
+            // 
+            this.técnicasDeConteoToolStripMenuItem.Name = "técnicasDeConteoToolStripMenuItem";
+            this.técnicasDeConteoToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.técnicasDeConteoToolStripMenuItem.Text = "Técnicas de Conteo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(319, 288);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "Form1";
+            this.Text = "Técnicas";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem técnicasDeConteoToolStripMenuItem;
     }
 }
 
---
+++ TecnicasConteo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+public class TecnicasConteo
+{
+    public MathTools mt;
+
+	public TecnicasConteo()
+	{
+        mt = new MathTools();
+	}
+
+    public int permutacionSinRepeticion(int n)
+    {
+        return mt.factorial(n);
+    }
+
+    public int permutacionConRepeticion(int n)
+    {
+
+    }
+
+    public int variacionSinRepeticion(int n, int m)
+    {
+        int divisor = mt.factorial(n - m);
+
+        return mt.factorial(n) / divisor;
+    }
+
+    public int variacionConRepeticion(int n, int m)
+    {
+        return (int) Math.Pow(n, m);
+    }
+
+    public int combinacionSinRepeticion(int n, int m)
+    {
+        int divisor = mt.factorial(m) * mt.factorial(n - m);
+
+        return mt.factorial(n) / divisor;
+    }
+
+    public int combinacionConRepeticion(int n, int m)
+    {
+        int numerador = mt.factorial(m + n - 1);
+        int denominador = mt.factorial(n) * mt.factorial(m - 1);
+
+        return numerador / denominador;
+    }
+
+    public int permutacionCircular(int n)
+    {
+        return mt.factorial(n - 1);
+    }
+
+    public int reglaMultiplicativa(int k)
+    {
+
+    }
+
+    public int reglaAditiva()
+    {
+
+    }
+}
+
+public class MathTools
+{
+    public MathTools()
+    {
+
+    }
+
+    public int factorial(int numero)
+    {
+        if (numero == 0)
+            return 1;
+
+        return numero * factorial(numero - 1);
+    }
+}
